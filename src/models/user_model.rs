07e465ad8b3c5bc6b639e5b8b6eb1e327fd92968
ryonakaya <nakayaryo0716@gmail.com>
2024-10-02 21:57:12 +0900

    [impl]データベース及びペイロードで使用するユーザデータの定義

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -1,13 +1,65 @@
-#[derive(Debug, Clone)]
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, FromRow, PartialEq)]
 pub struct User {
-    pub user_id: String,
+    user_id: String,
+    user_name: String,
+    user_mail: String,
+    user_pass: String,
+}
+
+impl User {
+    pub fn new(payload: CreateUserPayload) -> Self {
+        let user_id = Uuid::new_v4().to_string();
+        Self {
+            user_id,
+            user_name: payload.user_name,
+            user_mail: payload.user_mail,
+            user_pass: payload.user_pass,
+        }
+    }
+    
+    pub fn get_user_id(&self) -> &str {
+        &self.user_id
+    }
+    pub fn get_user_name(&self) -> &str {
+        &self.user_name
+    }
+    pub fn get_user_mail(&self) -> &str {
+        &self.user_mail
+    }
+    pub fn get_user_pass(&self) -> &str {
+        &self.user_pass
+    }
+
+}
+#[derive(Debug, Clone, Serialize, FromRow)]
+pub struct PubUserInfo {
+    user_id: String,
+    user_name: String,
+}
+
+impl PubUserInfo {
+    pub fn new(user_id: String, user_name: String) -> Self {
+        Self {
+            user_id,
+            user_name,
+        }
+    }
+    pub fn get_user_id(&self) -> &str {
+        &self.user_id
+    }
+
+    pub fn get_user_name(&self) -> &str {
+        &self.user_name
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CreateUserPayload {
     pub user_name: String,
     pub user_mail: String,
     pub user_pass: String,
-}
-
-#[derive(Debug, Clone)]
-pub struct PubUserInfo {
-    pub user_id: String,
-    pub user_name: String,
 }
