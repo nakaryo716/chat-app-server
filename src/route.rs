55a076d8d98d2f214031e2f6348c13f7b02fbb62
ryonakaya <nakayaryo0716@gmail.com>
2024-10-05 14:02:29 +0900

    [add]loginのエンドポンとを追加

--- src/route.rs
+++ src/route.rs
@@ -1,10 +1,11 @@
 use axum::{routing::post, Router};
 
-use crate::{database::users_db::UserDb, handlers::user_handlers::add_new_user};
+use crate::{database::users_db::UserDb, handlers::{auth_handlers::login, user_handlers::add_new_user}};
 
 // ルーティング処理の実装
 pub fn app(app_state: UserDb) -> Router {
     Router::new()
         .route("/user", post(add_new_user))
+        .route("/login", post(login))
         .with_state(app_state)
 }
