8f404b19da850433773fdc70b829a9d1a0d953e8
ryonakaya <nakayaryo0716@gmail.com>
2024-10-09 01:37:14 +0900

    [fix]ユーザー作成の前にペイロードのパスワードをハッシュ化するように変更

--- src/services/user.rs
+++ src/services/user.rs
@@ -1,7 +1,10 @@
+use argon2::{password_hash::Salt, Argon2, PasswordHasher};
+
 use crate::{
     database::users_db::{UserDataViewer, UserDbManage},
     models::user_model::{CreateUserPayload, PubUserInfo, User},
 };
+
 pub struct UserServices<'a, T>
 where
     T: UserDbManage<User, String> + UserDataViewer<String, String>,
@@ -28,7 +31,6 @@ where
         &self,
         new_user_payload: CreateUserPayload,
     ) -> Result<PubUserInfo, UserServiciesError> {
-        // TODO: payloadのバリデーション
         if let Ok(_) = self
             .db_pool
             .get_user_info_mail(new_user_payload.user_mail.clone())
@@ -36,9 +38,10 @@ where
         {
             return Err(UserServiciesError::AlreadyExist);
         }
-        // TODO: パスワードのHASH化
+        // パスワードのHASH化
+        let hashed_user_payload = UserServices::<T>::to_hash_pwd(new_user_payload)?;
         // ユーザーの作成
-        let new_user = User::new(new_user_payload);
+        let new_user = User::new(hashed_user_payload);
 
         // detabase呼び出し
         let user_info = self
@@ -50,26 +53,43 @@ where
     }
 
     pub async fn get_user_by_id(&self, user_id: String) -> Result<PubUserInfo, UserServiciesError> {
-        let a = self
-            .db_pool
-            .get_user_info_id(user_id)
-            .await
-            .map_err(|e| match e {
+        let user_info = self
+        .db_pool
+        .get_user_info_id(user_id)
+        .await
+        .map_err(|e| match e {
                 sqlx::error::Error::RowNotFound => UserServiciesError::NotFound,
                 _ => UserServiciesError::Unexpect,
             })?;
-        Ok(a)
+        Ok(user_info)
     }
 
     pub async fn delete_user(&self, user_id: &str) -> Result<(), UserServiciesError> {
         self.db_pool
-            .delete_user(user_id.to_string())
-            .await
-            .map_err(|e| match e {
-                sqlx::error::Error::RowNotFound => UserServiciesError::NotFound,
-                _ => UserServiciesError::Unexpect,
-            })?;
+        .delete_user(user_id.to_string())
+        .await
+        .map_err(|e| match e {
+            sqlx::error::Error::RowNotFound => UserServiciesError::NotFound,
+            _ => UserServiciesError::Unexpect,
+        })?;
         Ok(())
+    }
+
+    fn to_hash_pwd(payload: CreateUserPayload) -> Result<CreateUserPayload, UserServiciesError> {
+        let salt = dotenvy::var("SALT").map_err(|_| UserServiciesError::ToHash)?;
+        let salt = Salt::from_b64(&salt).map_err(|_| UserServiciesError::ToHash)?;
+    
+        let row_pwd = payload.user_pass;
+        let hashed_pwd = Argon2::default()
+            .hash_password(row_pwd.as_bytes(), salt)
+            .map_err(|_| UserServiciesError::ToHash)?
+            .to_string();
+    
+        let hashed_payload = CreateUserPayload {
+            user_pass: hashed_pwd,
+            ..payload
+        };
+        Ok(hashed_payload)
     }
 }
 
@@ -78,4 +98,5 @@ pub enum UserServiciesError {
     NotFound,
     AlreadyExist,
     Unexpect,
+    ToHash,
 }
