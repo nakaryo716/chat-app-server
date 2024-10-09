4a3092a3e34bb9e020bf09fe561794b1b6eae69c
ryonakaya <nakayaryo0716@gmail.com>
2024-10-09 22:13:36 +0900

    [fix]各ハンドラーのエラーをStatusCode -> UserServiciesError

--- src/handlers/user_handlers.rs
+++ src/handlers/user_handlers.rs
@@ -5,8 +5,7 @@ use crate::{
         user_model::{CreateUserPayload, PubUserInfo},
     },
     services::{
-        auth::{AuthorizeServices, COOKIEKEY},
-        user::{UserServices, UserServiciesError},
+        auth::{AuthError, AuthorizeServices, COOKIEKEY}, user::{UserServices, UserServiciesError}
     }, util::ValidatedJson,
 };
 use axum::{extract::State, response::IntoResponse, Json};
@@ -16,19 +15,18 @@ use http::StatusCode;
 pub async fn add_new_user(
     State(db): State<UserDb>,
     ValidatedJson(new_user_payload): ValidatedJson<CreateUserPayload>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, UserServiciesError> {
     let service = UserServices::new(&db);
     let user_info = service
         .create_new_user(new_user_payload)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .await?;
     Ok((StatusCode::OK, Json(user_info)))
 }
 
 pub async fn get_user_info_handle(
     claims: Claims,
     State(db): State<UserDb>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, UserServiciesError> {
     let user_service = UserServices::new(&db);
 
     let user_info = PubUserInfo::from(claims);
@@ -36,36 +34,34 @@ pub async fn get_user_info_handle(
 
     let query_res = user_service
         .get_user_by_id(user_id.to_string())
-        .await
-        .map_err(|e| match e {
-            UserServiciesError::NotFound => StatusCode::NOT_FOUND,
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
-        })?;
+        .await?;
 
     Ok((StatusCode::OK, Json(query_res)))
 }
 
-pub async fn delete_user_handle(
+pub async fn delete_user_handle<E>(
     claims: Claims,
     jar: CookieJar,
     State(db): State<UserDb>,
     ValidatedJson(auth_payload): ValidatedJson<AuthPayload>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, impl IntoResponse>
+where 
+    E: IntoResponse + From<AuthError> + From<UserServiciesError>,
+
+{
     // ユーザーデータの削除には再認証が必要
     let auth_service = AuthorizeServices::new(&db);
     auth_service
         .authorize(auth_payload)
-        .await
-        .map_err(|_| StatusCode::UNAUTHORIZED)?;
+        .await?;
 
     let user_service = UserServices::new(&db);
     let user_info = PubUserInfo::from(claims);
 
     user_service
         .delete_user(user_info.get_user_id())
-        .await
-        .map_err(|_| StatusCode::NOT_FOUND)?;
+        .await?;
 
     // JWTが残らないようにCookieから削除
-    Ok(jar.remove(Cookie::from(COOKIEKEY)))
+    Ok::<_, E>(jar.remove(Cookie::from(COOKIEKEY)))
 }
