3d89224bf52288bbd654ecb7dfde1d6291cc0130
ryonakaya <nakayaryo0716@gmail.com>
2024-10-09 22:14:13 +0900

    [fix]各ハンドラーのエラーをStatusCode -> RoomError

--- src/handlers/room_handlers.rs
+++ src/handlers/room_handlers.rs
@@ -1,5 +1,5 @@
 use crate::{
-    database::rooms_db::RoomDb,
+    database::rooms_db::{RoomDb, RoomError},
     models::{auth_model::Claims, rooms_model::CreateRoom, user_model::PubUserInfo},
     services::rooms::RoomServices, util::ValidatedJson,
 };
@@ -14,12 +14,11 @@ pub async fn create_room_handler(
     claims: Claims,
     State(room_db): State<RoomDb>,
     ValidatedJson(payload): ValidatedJson<CreateRoom>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, RoomError> {
     let room_services = RoomServices::new(&room_db);
     let user_info = PubUserInfo::from(claims);
     let room_info = room_services
-        .create_room(payload, user_info)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .create_room(payload, user_info)?;
     Ok((StatusCode::OK, Json(room_info)))
 }
 
@@ -27,22 +26,20 @@ pub async fn get_specific_room_info(
     _claims: Claims,
     State(room_db): State<RoomDb>,
     Path(room_id): Path<String>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, RoomError> {
     let room_services = RoomServices::new(&room_db);
     let room_info = room_services
-        .get_target_room_info(room_id.as_str())
-        .map_err(|_| StatusCode::NOT_FOUND)?;
+        .get_target_room_info(room_id.as_str())?;
     Ok((StatusCode::OK, Json(room_info)))
 }
 
 pub async fn get_all_room_info_handler(
     _claims: Claims,
     State(room_db): State<RoomDb>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, RoomError> {
     let room_services = RoomServices::new(&room_db);
     let room_infos = room_services
-        .get_all_room_info()
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .get_all_room_info()?;
     Ok((StatusCode::OK, Json(room_infos)))
 }
 
@@ -50,11 +47,10 @@ pub async fn delete_room_handler(
     claims: Claims,
     State(room_db): State<RoomDb>,
     Path(room_id): Path<String>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, RoomError> {
     let room_services = RoomServices::new(&room_db);
     let user_info = PubUserInfo::from(claims);
     room_services
-        .delete_owner_room(room_id.as_str(), user_info)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .delete_owner_room(room_id.as_str(), user_info)?;
     Ok(StatusCode::NO_CONTENT)
 }
