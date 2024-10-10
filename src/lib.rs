e29a21ccd4b6cff0bf1c4840cc9450a7412287b3
ryonakaya <nakayaryo0716@gmail.com>
2024-10-10 16:49:55 +0900

    [fix]libとmainを分けた(バイナリをディレクトリ名に依存しないようにするため)

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use axum::response::IntoResponse;
+use database::{rooms_db::RoomDb, users_db::UserDb};
+use services::{auth::AuthError, user::UserServiciesError};
+
+pub mod database;
+pub mod handlers;
+pub mod models;
+pub mod route;
+pub mod util;
+pub mod services;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    room_db: RoomDb,
+    user_db: UserDb,
+}
+
+impl AppState {
+    pub fn new(room_db: RoomDb, user_db: UserDb) -> Self {
+        Self { room_db, user_db }
+    }
+}
+#[derive(Debug)]
+pub enum AppError {
+    Auth(AuthError),
+    UserService(UserServiciesError),
+}
+
+impl From<AuthError> for AppError {
+    fn from(error: AuthError) -> Self {
+        AppError::Auth(error)
+    }
+}
+
+impl From<UserServiciesError> for AppError {
+    fn from(error: UserServiciesError) -> Self {
+        AppError::UserService(error)
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            AppError::Auth(err) => err.into_response(),
+            AppError::UserService(err) => err.into_response(),
+        }
+    }
+}
